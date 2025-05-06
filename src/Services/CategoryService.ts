54f46d48e5c213e97b1b5166363dfcac50beabe8
Your Name <javierconpec@gmail.com>
2025-05-06 14:29:41 -0500

    actu

--- src/Services/CategoryService.ts
+++ src/Services/CategoryService.ts
@@ -2,8 +2,8 @@ import { ICategory, ISubcategory } from "../Interfaces/category";
 import { fetchData } from "../Helpers/PeticionApi";
 
 
-export const getCategories = () => fetchData<ICategory[]>("/api/v1/categories", "GET");
+export const getCategories = () => fetchData<ICategory[]>("https://store8cores.onrender.com/api/v1/categories", "GET");
 
 
 export const getSubCategories = (categoryId: string) => 
-  fetchData<ISubcategory[]>(`/api/v1/categories/${categoryId}/subcategories`, "GET");
+  fetchData<ISubcategory[]>(`https://store8cores.onrender.com/api/v1/categories/${categoryId}/subcategories`, "GET");
